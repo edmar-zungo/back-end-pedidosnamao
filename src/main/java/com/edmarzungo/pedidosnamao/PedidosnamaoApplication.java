b84939f7871465af8041fb3a3343939a5d8ae012
edmar-zungo <edmarzungo0110@gmail.com>
2024-05-22 23:37:32 +0100

    comite inicial

---
+++ src/main/java/com/edmarzungo/pedidosnamao/PedidosnamaoApplication.java
@@ -0,0 +1,13 @@
+package com.edmarzungo.pedidosnamao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosnamaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosnamaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edmarzungo/pedidosnamao/PedidosnamaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmarzungo.pedidosnamao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosnamaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
